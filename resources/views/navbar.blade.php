1a7b1dd94b4f1af8d8630252a97186fcf65c28fc
ghania aqeela <118327385+araayeo@users.noreply.github.com>
2024-05-19 22:08:07 +0700

    navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,64 +8,52 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <style>
-.navbar-custom {
-  background: linear-gradient(45deg, rgb(133, 208, 237), rgb(170, 170, 246));
-  position: sticky;
-  top: 0;
-  z-index: 1;
-}
+  body {
+  background-image: url("{{asset('publicimage/background.jpeg')}}");
+  background-size: cover;
+  background-attachment: fixed;
+  }
+  
+  .navbar-custom {
+    background: linear-gradient(70deg, rgb(135,206, 250) 20%, rgb(0,191,255));
+  }
 
-.dropdown {
-  position: relative;
-}
+  .navbar-brand img {
+    height: 60px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
 
-.dropdown-icon {
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-}
+  .navbar-brand-text{
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 10px
+  }
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-  z-index: 1;
-  right: 0;
-}
-
-.dropdown-content li {
-  padding: 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content li:hover {
-  background-color: #f1f1f1;
-}
-
-.dropdown:hover .dropdown-content {
- display: block;
-}
-
+  .nav-link {
+    font-size: 20px;
+  }
 </style>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
-    <div class="container-fluid">
-        <a class="navbar-brand"  href="#">Aurora Photo Studio</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto ">
-                <li class="nav-item"><a class="nav-link" href="#">Booking</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Details</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark navbar-custom">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="{{ asset('publicimage/aurorastudio.jpg') }}" alt="Logo">
+            <span class="navbar-brand-text">Aurora Photo Studio</span>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                  <li class="nav-item"><a class="nav-link" href="#">Booking</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Details</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Account</a></li>
+              </ul>
+          </div>
+      </div>
+  </nav>
 
 @yield('content')
 
